5cf0e1e673b9c15cbd91b46f34e48f342f6fe0c6
rob_andrew_ellis <robandrewellis@gmail.com>
2024-08-28 10:45:58 +0100

    initial commit

---
+++ rules.mk
@@ -0,0 +1,8 @@
+OLED_ENABLE = no
+ENCODER_ENABLE = no		  # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = no    # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+SPLIT_KEYBOARD = yes      # ??
+SERIAL_DRIVER = vendor    # ??
+BOOTLOADER = rp2040
+CONVERT_TO = promicro_rp2040
